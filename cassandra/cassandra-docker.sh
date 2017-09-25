0fa5121a5cd3d9269dc9a9fe8c833cb6777fff89
mwringe <mwringe@redhat.com>
2017-09-25 18:09:48 -0400

    Specify the Cassandra HEAP_NEWSIZE based only on the MAX_HEAP_SIZE

--- cassandra/cassandra-docker.sh
+++ cassandra/cassandra-docker.sh
@@ -172,12 +172,9 @@ else
   echo "The MAX_HEAP_SIZE envar is set to ${MAX_HEAP_SIZE}. Using this value"
 fi
 
-if [ -z "${HEAP_NEWSIZE}" ] && [ -z "${CPU_LIMIT}" ]; then
-  echo "The HEAP_NEWSIZE and CPU_LIMIT envars are not set. Defaulting the HEAP_NEWSIZE to 100M"
-  export HEAP_NEWSIZE=100M
-elif [ -z "${HEAP_NEWSIZE}" ]; then
-  export HEAP_NEWSIZE=$((CPU_LIMIT/10))M
-  echo "THE HEAP_NEWSIZE envar is not set. Setting to ${HEAP_NEWSIZE} based on the CPU_LIMIT of ${CPU_LIMIT}. [100M per CPU core]"
+if [ -z "${HEAP_NEWSIZE}" ]; then
+  export HEAP_NEWSIZE="$(( ${MAX_HEAP_SIZE::-1} / 3 ))M"
+  echo "The HEAP_NEWSIZE envar is not set. Setting the HEAP_NEWSIZE to one third the MAX_HEAP_SIZE: ${HEAP_NEWSIZE}"
 else
   echo "The HEAP_NEWSIZE envar is set to ${HEAP_NEWSIZE}. Using this value"
 fi
