c94c1b565465726ef49000269b145e2548d7381d
ruben.vargas <ruben.vp8510@gmail.com>
2018-04-26 09:21:09 -0500

    Fix schema image names so they can match with the Ansible installer.

--- hack/build-images.sh
+++ hack/build-images.sh
@@ -78,7 +78,7 @@ if [ "$verbose" = true ]; then
   set -x
 fi
 
-for component in deployer heapster hawkular-metrics hawkular-metrics-schema cassandra; do
+for component in deployer heapster hawkular-metrics schema-installer cassandra; do
   BUILD_STARTTIME=$(date +%s)
   comp_path=$source_root/$component/
   docker_tag=${prefix}metrics-${component}:${version}
